3f464db9f0814da64b9a78a59e6006b956034563
apades <36807043+apades@users.noreply.github.com>
2024-04-23 17:32:53 +0800

    fix: vite-plugin导致的循环引用问题修复

--- src/core/WebProvider/index.ts
+++ src/core/WebProvider/index.ts
@@ -1,4 +1,8 @@
+// ! 用单独一个文件来解决循环引用问题
+// 其他文件不要直接 import WebProvider from './WebProvider'
+// 改成import { WebProvider } from './'
 import WebProvider from './WebProvider'
 import DocWebProvider from './DocWebProvider'
+import CanvasWebProvider from './CanvasWebProvider'
 
-export { WebProvider, DocWebProvider }
+export { WebProvider, DocWebProvider, CanvasWebProvider }
